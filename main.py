7294a4c7d1e8f0820aaf7f3015911bf769d1fb2e
Thomas Su <thomaswsu@me.com>
2021-01-25 11:25:13 -0800

    intial outline

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas
+import datetime
+
+def valueELI(issuePrice: float, intialFixingDate: datetime.date, finalFixingDate: datetime.date, finalRedemptionDate: datetime.date) -> float:
+    return(0)
