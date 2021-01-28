2853121935bee0c00b96f03d10f63ce40a6b4a58
Thomas Su <thomaswsu@me.com>
2021-01-27 18:59:50 -0800

    resolve merge conflicts

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas
 from matplotlib import pyplot as plt 
 import matplotlib.dates as mdates
 from matplotlib.dates import DateFormatter
-#import pandas_datareader.data as pdr
 import numpy as np
 from pandas.core.frame import DataFrame
 from scipy import stats
@@ -12,8 +11,9 @@ import math as m
 import time
 import random
 import numpy as np
+import pandas_market_calendars as mcal
 
-def getFinalRedemption(price1: float, price2: float, price: float):
+def getFinalRedemption(price1: float, price2: float, price3: float):
     
     finalLevel=np.array([price1,price2,price3])
     par=np.array([23723.38,11079.79, 8846.449])
@@ -87,7 +87,6 @@ def monteCarlo(iterations: int, days: int, underlying: pandas.DataFrame):
         
     return ul_price_df
 
-
 def daysAfter(start, end):
     """ accepts format m/d/y"""
     start=pandas.to_datetime(start,format= '%m/%d/%Y')
@@ -133,24 +132,30 @@ def notepath(ul1, ul2, ul3, payoutperiod):
             timeinpayoutperiod-=1
     return(payoutlist)
 
-def earlyRedeem(days: list, priceHistories: list, ticker: str, interestRate: float, underlying: pandas.DataFrame, startDate: pandas.DatetimeIndex) -> float:
+def earlyRedeem(days: list, priceHistories: list, tickers: list, interestRate: float, underlying: pandas.DataFrame, startDate: pandas.DatetimeIndex) -> float:
     """
     Pruce histories is a 2d list. It stores the the price history of each respective tickers
+    tickers is a list of strings
     """
     intialValues = []
     for index in priceHistories:
         intialValues.append(index[0])
-    calendar = mcal.get_calendar(ticker)
 
     triggerObservationDays = pandas.Series(["7/7/2020", "10/7/2020", "1/7/2021", "4/7/2021", "7/7/2021", "10/7/2021", "1/7/2022", "4/7/2022", "7/7/2022", "10/7/2022"])
 
     dayDifferences = []
-    # for intial 
-    # for day in triggerObservationDays:
-    #     dates = calendar.schedule(startDate, day)
-    #     dayDifference = len(dates)
-    for i in range(len(intialValues)): 
-        priceHistory[i][dayDifference] >= intialValues[i] * 0.97:
+    for ticker in tickers:
+        calendar = mcal.get_calendar(ticker)
+        dayDifferencesForTicker = []
+        for day in triggerObservationDays:
+            dates = calendar.schedule(startDate, day)
+            dayDifference = len(dates)
+            dayDifferencesForTicker.append(dayDifference)
+        dayDifferences.append(dayDifferencesForTicker)
+
+    for i in range(len(priceHistories)):
+        for j in range(len(intialValues)): 
+            priceHistory[i][dayDifference] >= intialValues[i] * 0.97:
 
         
 
@@ -161,6 +166,9 @@ def earlyRedeem(days: list, priceHistories: list, ticker: str, interestRate: flo
     return()
 
 def overrideDates(monteCarloSimulation: pandas.DataFrame, ticker: str, startDate: str, endDate: str) -> pandas.DataFrame:
+    """
+    Probably a function that we don't need anymore
+    """
     calendar = mcal.get_calendar(ticker)
     dates = calendar.schedule(startDate)
     monteCarloSimulation["date"] = dates
@@ -174,31 +182,29 @@ def valueELI(issuePrice: float, intialFixingDate: date, finalFixingDate: date, f
 
 
 if __name__ == "__main__":
-  
+    random.seed(2021) # set seed for random number generator for monte carlo
+
     start="24/01/2011"
     end="16/03/2020"
-    
-
     names = ["FTSE MIB", "Hang Seng CEI", "Nasdaq 100"]
     calendarTickers = ["", "", ""]
 
     # Get past market prices MILAN
-    
     FTSEMIB = getIndexPrice(ticker="FTSE MIB", country="Italy", startDate=start, endDate=end)
     HSCEI = getIndexPrice(ticker="Hang Seng CEI", country="Hong Kong", startDate=start, endDate=end)
     NDX = getIndexPrice(ticker="Nasdaq 100 ", country="United States", startDate=start, endDate=end)
-        
-    
+
+    indexes = [FTSEMIB, HSCEI, NDX]
 
     # Convert price data to list because I know how to use lists 
-    FTSEMIB_priceList = list(FTSEMIB["Open"])
-    HSCEI_priceList = list(HSCEI["Open"])
-    NDX_priceList = list(NDX["Open"])
+    priceLists = []
+    for index in indexes:
+        priceLists.append(list(index["Open"]))
 
     # Get dates for each index
-    FTSEMIB_dates = list(pandas.DatetimeIndex.to_pydatetime(FTSEMIB.index))
-    HSCEI_dates = list(pandas.DatetimeIndex.to_pydatetime(HSCEI.index))
-    NDX_dates = list(pandas.DatetimeIndex.to_pydatetime(NDX.index))
+    dates = []
+    for index in indexes:
+        dates.append(list(pandas.DatetimeIndex.to_pydatetime(index.index))) # poor naming...
 
     # Plot FTSEMIB for visual context 
     fig, ax = plt.subplots()
@@ -208,7 +214,7 @@ if __name__ == "__main__":
     months = mdates.MonthLocator()  # every month
     years_fmt = mdates.DateFormatter('%Y')
 
-    plt.plot_date(FTSEMIB_dates, FTSEMIB_priceList, 'b-')
+    plt.plot_date(dates[1], priceLists[1], 'b-')
     formatter = DateFormatter('%m/%d/%y')
     ax.xaxis.set_major_locator(years)
     ax.xaxis.set_major_formatter(years_fmt)
@@ -216,31 +222,38 @@ if __name__ == "__main__":
     ax.grid(True)
 
     # Make best fit line 
-    FTSEMIB_xAxis = range(0, len(FTSEMIB_dates)) # We need this to calculate line of best line 
-    FTSEMIB_m, FTSEMIB_b, FTSEMIB_r_value, FTSEMIB_p_value, FTSEMIB_std_err = stats.linregress(FTSEMIB_xAxis, FTSEMIB_priceList)
-    plt.plot(FTSEMIB_dates, FTSEMIB_m * FTSEMIB_xAxis + FTSEMIB_b)
+    xAxies = []
+    statistics = []
+    for i in range(len(indexes)):
+        xAxies.append(range(0, len(dates[i])))
+        slope, b, r_value, p_value, std_err = stats.linregress(xAxies[i], priceLists[i])
+        statistics.append([slope, b, r_value, p_value, std_err])
+
+    # statistics[1][0] is slope
+    # statistics[1][0] is b
+    plt.plot(dates[1], statistics[1][0] * xAxies[1] + statistics[1][1])
     
-    FTSEMIB_std_dev = FTSEMIB_std_err * m.sqrt(len(FTSEMIB_dates))
+    standardDeviations = []
+    for i in range(len(indexes)):
+        standardDeviations.append(statistics[i][4])
 
     # Actually plot
-    #fig.autofmt_xdate()
-    #plt.show()
+    fig.autofmt_xdate()
+    plt.show()
 
-    # Calculate Standard Deviation of the remaining two assets in portfolio
-    HSCEI_xAxis = range(0, len(HSCEI_dates))
-    HSCEI_m, HSCEI_b, HSCEI_r_value, HSCEI_p_value, HSCEI_std_err = stats.linregress(HSCEI_xAxis, HSCEI_priceList)
-    HSCEI_std_dev = HSCEI_std_err * m.sqrt(len(HSCEI_dates))
-
-    NDX_xAxis = range(0, len(NDX_dates))
-    NDX_m, NDX_b, NDX_r_value, NDX_p_value, NDX_std_err = stats.linregress(NDX_xAxis, NDX_priceList)
-    NDX_std_dev = NDX_std_err * m.sqrt(len(NDX_dates))
 
     # Print some stats for now
-    print("FTSEMIB Slope: {}, FTSEMIB Standard Deviation: {}".format(FTSEMIB_m, FTSEMIB_std_dev))
-    print("HSCEI Slope: {}, HSCEI Standard Deviation: {}".format(HSCEI_m, HSCEI_std_dev))
-    print("NDX Slope: {}, NDX Standard Deviation: {}".format(NDX_m, NDX_std_dev))
+    for i in range(len(names)):
+        print("{} Slope: {}, {} Standard Deviation: {}".format(names[i], statistics[i][0], names[i], standardDeviations[i]))
+
+    #Example Test
+    f=monteCarlo(500, 252, HSCEI)
     
     
+    fig=plt.figure()
+    plt.plot(f)
+    plt.show()
+
     daynum=1029
     simnum=10
     
@@ -259,7 +272,6 @@ if __name__ == "__main__":
     
     #if you want to see the distribution of final prices
     #plt.hist(b.iloc[-1],bins='auto')
-
 
 """ 
 Sources:
